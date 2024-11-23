792d550cf2de1d703c86a44559ed9981f8a27c5b
henriqueramalho1 <henrique.ramalho2002@gmail.com>
2024-11-23 19:25:46 -0300

    fix: measure test

--- embedded/main.py
+++ embedded/main.py
@@ -5,6 +5,7 @@ import queue
 from embedded.audio import capture_audio, play_audio
 from embedded.client_recognition import recognize_customer, generate_new_encodings
 from embedded.motors import motor_task
+from embedded.measure_coffee import dispense_task
 
 if __name__ == "__main__":
     audio_queue = queue.Queue()
@@ -12,16 +13,18 @@ if __name__ == "__main__":
 
     turn_on_motor_event_flag = threading.Event
     coffee_container = multiprocessing.Value('i', 1)
+    measure_coffee_queue = queue.Queue
 
-    print("Queue init")
-    threading.Thread(target=capture_audio, daemon=True, args=(audio_queue,)).start()
+    #threading.Thread(target=capture_audio, daemon=True, args=(audio_queue,)).start()
     threading.Thread(target=motor_task, daemon=True, args=(turn_on_motor_event_flag, coffee_container))
+    threading.Thread(target=dispense_task, daemon=True, args=(measure_coffee_queue, coffee_container, turn_on_motor_event_flag))
     #threading.Thread(target=play_audio, daemon=True, args=(audio_queue,)).start()
     #threading.Thread(target=recognize_customer, daemon=True).start()
     #threading.Thread(target=generate_new_encodings, daemon=True, args=(rebuild_binaries_event,)).start()
 
     try:
         while True:
-            time.sleep(1)
+            measure_coffee_queue.put({"container_id": 2, "weight": 100})
+            time.sleep(10000)
     except KeyboardInterrupt:
         print("Finishing...")
--- embedded/measure_coffee.py
+++ embedded/measure_coffee.py
@@ -7,7 +7,7 @@ DT_PIN = 5
 SCK_PIN = 6
 
 
-def dispense_task(measure_coffee_queue: queue.Queue, turn_on_motor_event_flag: threading.Event):
+def dispense_task(measure_coffee_queue: queue.Queue, coffee_container, turn_on_motor_event_flag: threading.Event):
     hx = HX711(DT_PIN, SCK_PIN)
     hx.set_reading_format("MSB", "MSB")
     hx.reset()
@@ -18,6 +18,9 @@ def dispense_task(measure_coffee_queue: queue.Queue, turn_on_motor_event_flag: t
         measure_coffee_request = measure_coffee_queue.get()
 
         container_id = measure_coffee_request["container"]
+        with coffee_container.get_lock():
+            coffee_container.value = container_id
+
         requested_coffee_weight = measure_coffee_request["weight"]
         turn_on_motor_event_flag.set()
 
@@ -25,10 +28,10 @@ def dispense_task(measure_coffee_queue: queue.Queue, turn_on_motor_event_flag: t
             weight = max(0,int(hx.get_weight(5)))
             hx.power_down()
             hx.power_up()
+            time.sleep(0.1)
 
         turn_on_motor_event_flag.clear()
-
-        time.sleep(0.1)
+        
 
 
         
