9ee8efdf58f4d6c662d9e2fb896bdf2bbba1eb1c
apenasetore <etoremt@gmail.com>
2024-11-26 21:07:53 +0000

    :egg: Chat Voice

--- Voice/coffeChat.py
+++ Voice/coffeChat.py
@@ -4,15 +4,15 @@ import edge_tts
 import io
 import voice_detection as vd
 import os
+import json
 
 from dotenv import load_dotenv
 from pathlib import Path
 from openai import OpenAI
-
+from pydantic import BaseModel
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-print(API_KEY)
 client = OpenAI(
   api_key=API_KEY,
 )
@@ -26,23 +26,33 @@ def transcribe_audio(file_path):
         model="whisper-1",
         file=file
     )
-    print(transcript.text)
     return transcript.text
 
 
 # Function to generate a response using ChatGPT
-def generate_response(transcription):
-    prompt = f"The user said: '{transcription}'. Please respond to this appropriately."
+def generate_response(messages, phase_prompt):
+
+    class ResponseFormat(BaseModel):
+        inPhase: bool
+        message: str
     
-    response = client.chat.completions.create(
+    response = client.beta.chat.completions.parse(
                 model="gpt-4o",
                 messages=[
-                    {"role": "system", "content": "You are a coffee grain vending machine with 4 types of grains available repond to this text."},
-                    {"role": "user", "content": transcription}
-                    ]
-                )
-    print(response.choices[0].message.content)
-    return response.choices[0].message.content
+                    {"role": "system", "content": f'''You are a coffee grain vending machine with 4 types of grains available. 
+                     Respond to this text according to the phase instructions. 
+                     {phase_prompt}.
+                     Continue this convesation with the user.
+                     {messages}'''},
+                    ],
+                response_format=ResponseFormat
+    )
+    response = json.loads(response.choices[0].message.content)
+    print(response)
+    in_phase = response['inPhase']
+    message = response['message'] 
+    print(in_phase, message)
+    return in_phase, message
 
 # Function to convert text to speech using OpenAI's TTS API
 def synthesize_speech(text):
@@ -85,6 +95,61 @@ def main(file_path):
     # print('All Time',end_time_3-start_time)
         
 
-        # Example usage
-vd.voice_detection()
-main("gravacao.wav")
+if __name__ == "__main__":
+
+    prompt = [
+    {
+        "name": "IntroductionState",
+        "goal": "Present to the user and ask how you can help.",
+        "guideline": "Say hello and welcome the guest to the machine. Keep the conversation natural. Call them by their name, if in client mode."
+    },
+    {
+        "name": "AvailableCoffeeState",
+        "goal": "Figure out what is the user's coffee of interest.",
+        "guideline": "Talk to the user about which of the available coffees suits their taste. You can use the database to see previous purchases, if known, or to get more information on a specific type of coffee."
+    },
+    {
+        "name": "QuantityState",
+        "goal": "Get how much of the coffee the consumer wants, considering the bounds.",
+        "guideline": "Ask about the quantity of grams the user wants. The limit is from 20 to 300 grams. Do not allow the user to get out of those bounds."
+    },
+    {
+        "name": "PaymentState",
+        "goal": "Get confirmation from the user about the order and payment.",
+        "guideline": "Confirm the order with the user by repeating it for them. Tell the price and ask for confirmation."
+    },
+    {
+        "name": "RegisterState",
+        "goal": "Get first and last name of the customer.",
+        "guideline": "Ask if the guest wants to register as a customer; if not, close the phase. If yes, get their first and last name."
+    },
+    {
+        "name": "GoodbyeState",
+        "goal": "Finish order and say goodbye.",
+        "guideline": "Say goodbye to the customer."
+    }
+    ]
+
+    messages = []
+    for state in prompt:
+        print(messages)
+        print(f"State: {state['name']}")
+        print(f"Goal: {state['goal']}")
+        print(f"Guideline: {state['guideline']}")
+        print("\n")
+        phase_prompt = f"Verify if the current phase is {state['name']}. The phase's goal is {state['goal']}. Return true in inPhase in case the phase goal has not been accomplished. Return false in case the objective has been accomplished. To reach the goal, you must {state['guideline']}."
+        
+        in_phase, message = generate_response(messages, phase_prompt)
+        if (in_phase):
+            messages.append(f"Machine: {message}")
+            print(messages)
+        else:
+            break
+        
+        vd.voice_detection()
+        transcription = transcribe_audio('gravacao.wav')
+        print(transcription)
+        messages.append(f"Client: {transcription}")
+
+        
+        
