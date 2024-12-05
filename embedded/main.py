0adf591b9c2c95d61e0c7dc0b80a7bf06d769bd2
apenasetore <etoremt@gmail.com>
2024-12-05 21:58:37 +0000

    fix: motors

--- embedded/main.py
+++ embedded/main.py
@@ -6,7 +6,7 @@ from embedded.audio import capture_audio
 from embedded.client_recognition import recognize_customer, generate_new_encodings
 from embedded.gpt import generate_response
 
-# from embedded.motors import motor_task
+from embedded.motors import clean_motors, motor_task
 
 # from embedded.measure_coffee import dispense_task
 
@@ -18,45 +18,46 @@ if __name__ == "__main__":
     turn_on_motor_event_flag = multiprocessing.Event()
     capture_audio_event_flag = multiprocessing.Event()
 
-    coffee_container = multiprocessing.Value("i", 1)
+    coffee_container = multiprocessing.Value("i", 0)
 
     measure_coffee_queue = multiprocessing.Queue()
     customer_queue = multiprocessing.Queue()
     audio_queue = multiprocessing.Queue()
 
-    turn_on_motor_event_flag.clear()
+    turn_on_motor_event_flag.set()
     # multiprocessing.Process(
     #     target=generate_new_encodings,
     #     daemon=True,
     #     args=(generate_new_encodings_event_flag,),
     # ).start()
-    # multiprocessing.Process(target=motor_task, daemon=True, args=(turn_on_motor_event_flag, coffee_container)).start()
+    multiprocessing.Process(target=motor_task, daemon=True, args=(turn_on_motor_event_flag, coffee_container)).start()
     # multiprocessing.Process(target=dispense_task, daemon=True, args=(measure_coffee_queue, coffee_container, turn_on_motor_event_flag)).start()
-    multiprocessing.Process(
-        target=recognize_customer,
-        args=(recognize_customer_event_flag, customer_queue),
-        daemon=True,
-    ).start()
-    multiprocessing.Process(
-        target=capture_audio, daemon=True, args=(audio_queue, capture_audio_event_flag)
-    ).start()
-    multiprocessing.Process(
-        target=generate_response,
-        daemon=True,
-        args=(
-            customer_queue,
-            audio_queue,
-            measure_coffee_queue,
-            capture_audio_event_flag,
-            recognize_customer_event_flag,
-        ),
-    ).start()
+    # multiprocessing.Process(
+    #     target=recognize_customer,
+    #     args=(recognize_customer_event_flag, customer_queue),
+    #     daemon=True,
+    # ).start()
+    # multiprocessing.Process(
+    #     target=capture_audio, daemon=True, args=(audio_queue, capture_audio_event_flag)
+    # ).start()
+    # multiprocessing.Process(
+    #     target=generate_response,
+    #     daemon=True,
+    #     args=(
+    #         customer_queue,
+    #         audio_queue,
+    #         measure_coffee_queue,
+    #         capture_audio_event_flag,
+    #         recognize_customer_event_flag,
+    #     ),
+    # ).start()
 
     try:
         while True:
             print("System on")
-            time.sleep(2)
-            recognize_customer_event_flag.set()
+            #recognize_customer_event_flag.set()
             time.sleep(100000)
     except KeyboardInterrupt:
         print("Finishing...")
+    finally:
+        time.sleep(2)
--- embedded/motors.py
+++ embedded/motors.py
@@ -5,13 +5,10 @@ import time
 
 # PINS
 M1_STEP_PIN = 5
-M2_STEP_PIN = 2
-M3_STEP_PIN = 12
-M4_STEP_PIN = 20
-DIR_PIN = 6
-MS1_PIN = 26 
-MS2_PIN = 19
-MS3_PIN = 13
+M2_STEP_PIN = 6
+M3_STEP_PIN = 13
+M4_STEP_PIN = 26
+DIR_PIN = 12
 
 
 def setup():
@@ -20,16 +17,18 @@ def setup():
     GPIO.setup(M2_STEP_PIN, GPIO.OUT)
     GPIO.setup(M3_STEP_PIN, GPIO.OUT)
     GPIO.setup(M4_STEP_PIN, GPIO.OUT)
+    
     GPIO.setup(DIR_PIN, GPIO.OUT)
-    GPIO.setup(MS1_PIN, GPIO.OUT)
-    GPIO.setup(MS2_PIN, GPIO.OUT)
-    GPIO.setup(MS3_PIN, GPIO.OUT)
-
-
     GPIO.output(DIR_PIN, GPIO.HIGH)
-    GPIO.output(MS1_PIN, GPIO.LOW)
-    GPIO.output(MS2_PIN, GPIO.LOW)
-    GPIO.output(MS3_PIN, GPIO.LOW)
+
+    clean_motors()
+
+def clean_motors():
+    GPIO.output(M1_STEP_PIN, GPIO.LOW)
+    GPIO.output(M2_STEP_PIN, GPIO.LOW)
+    GPIO.output(M3_STEP_PIN, GPIO.LOW)
+    GPIO.output(M4_STEP_PIN, GPIO.LOW)
+    print("Cleaned motors")
 
 
 def motor_task(turn_on_motor_event_flag, coffee_container):
@@ -38,41 +37,48 @@ def motor_task(turn_on_motor_event_flag, coffee_container):
     coffee_configs = [M1_STEP_PIN, M2_STEP_PIN, M3_STEP_PIN, M4_STEP_PIN]
     delay = 0.0005
 
-    while True:
-        print("Waiting for event flag")
-        while not turn_on_motor_event_flag.is_set():
-            pass
+    try:
+        while True:
+            print("Waiting for event flag")
+            while not turn_on_motor_event_flag.is_set():
+                pass
 
-        with coffee_container.get_lock():
-            print(f"Loading config of {coffee_container.value=}")
-            coffee_index = coffee_container.value
 
-            if coffee_index > 3 or coffee_index < 0:
-                coffee_index = 0
+            with coffee_container.get_lock():
+                print(f"Loading config of {coffee_container.value=}")
+                coffee_index = coffee_container.value
 
-            step_pin = coffee_configs[coffee_index]
+                if coffee_index > 3 or coffee_index < 0:
+                    coffee_index = 0
 
-        if turn_on_motor_event_flag.is_set():
-            print(f"Forward {step_pin}")
-            for i in range(400):
-                if not turn_on_motor_event_flag.is_set():
-                    break
-                GPIO.output(DIR_PIN, GPIO.HIGH)
-                GPIO.output(step_pin, GPIO.HIGH)
-                time.sleep(delay)
-                GPIO.output(step_pin, GPIO.LOW)
-                time.sleep(delay)
+                step_pin = coffee_configs[coffee_index]
 
-        time.sleep(0.1)
+            if turn_on_motor_event_flag.is_set():
+                print(f"Forward {step_pin}")
+                for i in range(400):
+                    if not turn_on_motor_event_flag.is_set():
+                        break
+                    GPIO.output(DIR_PIN, GPIO.HIGH)
+                    GPIO.output(step_pin, GPIO.HIGH)
+                    time.sleep(delay)
+                    GPIO.output(step_pin, GPIO.LOW)
+                    time.sleep(delay)
 
-        if turn_on_motor_event_flag.is_set():
-            print("Backwards")
-            for i in range(300):
-                if not turn_on_motor_event_flag.is_set():
-                    break
+            time.sleep(0.1)
 
-                GPIO.output(DIR_PIN, GPIO.LOW)
-                GPIO.output(step_pin, GPIO.HIGH)
-                time.sleep(delay)
-                GPIO.output(step_pin, GPIO.LOW)
-                time.sleep(delay)
+            if turn_on_motor_event_flag.is_set():
+                print("Backwards")
+                for i in range(300):
+                    if not turn_on_motor_event_flag.is_set():
+                        break
+
+                    GPIO.output(DIR_PIN, GPIO.LOW)
+                    GPIO.output(step_pin, GPIO.HIGH)
+                    time.sleep(delay)
+                    GPIO.output(step_pin, GPIO.LOW)
+                    time.sleep(delay)
+
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("Cleaning motors")
+        clean_motors()
