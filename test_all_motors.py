3ff8ea96ac8ca3d6c994013d45211d1b63748b93
apenasetore <etoremt@gmail.com>
2024-12-10 20:22:44 +0000

    add: tests

---
+++ test_all_motors.py
@@ -0,0 +1,61 @@
+import RPi.GPIO as GPIO
+import time
+
+# Configuração dos pinos GPIO
+DIR_PIN = 12  # Pino para definir a direção
+M1_STEP_PIN = 5  # Pino para enviar os pulsos de passo
+M2_STEP_PIN = 6
+M3_STEP_PIN = 13
+M4_STEP_PIN = 26
+
+# Configuração dos GPIOs
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(DIR_PIN, GPIO.OUT)
+GPIO.setup(M1_STEP_PIN, GPIO.OUT)
+GPIO.setup(M2_STEP_PIN, GPIO.OUT)
+GPIO.setup(M3_STEP_PIN, GPIO.OUT)
+GPIO.setup(M4_STEP_PIN, GPIO.OUT)
+
+
+GPIO.output(M1_STEP_PIN, GPIO.LOW)
+GPIO.output(M2_STEP_PIN, GPIO.LOW)
+GPIO.output(M3_STEP_PIN, GPIO.LOW)
+GPIO.output(M4_STEP_PIN, GPIO.LOW)
+
+motors = [M1_STEP_PIN, M2_STEP_PIN, M3_STEP_PIN, M4_STEP_PIN]
+
+try:
+    while True:
+
+        for motor in motors:
+
+            for i in range(5):
+                GPIO.output(DIR_PIN, GPIO.HIGH)
+
+                for _ in range(100):  # 200 passos (ajuste conforme necessário)
+                    GPIO.output(motor, GPIO.HIGH)
+                    time.sleep(0.001)  # Tempo HIGH (ajuste para controle de velocidade)
+                    GPIO.output(motor, GPIO.LOW)
+                    time.sleep(0.001)  # Tempo LOW
+                    print("Rotating")
+
+
+                GPIO.output(DIR_PIN, GPIO.LOW) 
+
+                for _ in range(50):  # 200 passos (ajuste conforme necessário)
+                    GPIO.output(motor, GPIO.HIGH)
+                    time.sleep(0.001)  # Tempo HIGH (ajuste para controle de velocidade)
+                    GPIO.output(motor, GPIO.LOW)
+                    time.sleep(0.001)  # Tempo LOW
+                    print("Rotating")
+            
+            time.sleep(0.5)
+            
+except KeyboardInterrupt:
+    print("Parando...")
+finally:
+    GPIO.output(M1_STEP_PIN, GPIO.LOW)
+    GPIO.output(M2_STEP_PIN, GPIO.LOW)
+    GPIO.output(M3_STEP_PIN, GPIO.LOW)
+    GPIO.output(M4_STEP_PIN, GPIO.LOW)
--- test_motor.py
+++ test_motor.py
@@ -2,13 +2,26 @@ import RPi.GPIO as GPIO
 import time
 
 # Configuração dos pinos GPIO
-DIR_PIN = 5  # Pino para definir a direção
-STEP_PIN = 6  # Pino para enviar os pulsos de passo
+DIR_PIN = 12  # Pino para definir a direção
+M1_STEP_PIN = 5  # Pino para enviar os pulsos de passo
+M2_STEP_PIN = 6
+M3_STEP_PIN = 13
+M4_STEP_PIN = 26
 
 # Configuração dos GPIOs
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(DIR_PIN, GPIO.OUT)
-GPIO.setup(STEP_PIN, GPIO.OUT)
+GPIO.setup(M1_STEP_PIN, GPIO.OUT)
+GPIO.setup(M2_STEP_PIN, GPIO.OUT)
+GPIO.setup(M3_STEP_PIN, GPIO.OUT)
+GPIO.setup(M4_STEP_PIN, GPIO.OUT)
+
+
+GPIO.output(M1_STEP_PIN, GPIO.LOW)
+GPIO.output(M2_STEP_PIN, GPIO.LOW)
+GPIO.output(M3_STEP_PIN, GPIO.LOW)
+GPIO.output(M4_STEP_PIN, GPIO.LOW)
 
 # Configuração da direção (True para frente, False para trás)
  # Ajuste para frente
@@ -20,9 +33,9 @@ try:
         GPIO.output(DIR_PIN, GPIO.HIGH)
 
         for _ in range(100):  # 200 passos (ajuste conforme necessário)
-            GPIO.output(STEP_PIN, GPIO.HIGH)
+            GPIO.output(M4_STEP_PIN, GPIO.HIGH)
             time.sleep(0.001)  # Tempo HIGH (ajuste para controle de velocidade)
-            GPIO.output(STEP_PIN, GPIO.LOW)
+            GPIO.output(M4_STEP_PIN, GPIO.LOW)
             time.sleep(0.001)  # Tempo LOW
             print("Rotating")
 
@@ -30,13 +43,16 @@ try:
         GPIO.output(DIR_PIN, GPIO.LOW) 
 
         for _ in range(50):  # 200 passos (ajuste conforme necessário)
-            GPIO.output(STEP_PIN, GPIO.HIGH)
+            GPIO.output(M4_STEP_PIN, GPIO.HIGH)
             time.sleep(0.001)  # Tempo HIGH (ajuste para controle de velocidade)
-            GPIO.output(STEP_PIN, GPIO.LOW)
+            GPIO.output(M4_STEP_PIN, GPIO.LOW)
             time.sleep(0.001)  # Tempo LOW
             print("Rotating")
             
 except KeyboardInterrupt:
     print("Parando...")
 finally:
-    GPIO.output(STEP_PIN, GPIO.LOW)  # Limpa a configuração dos pinos GPIO
+    GPIO.output(M1_STEP_PIN, GPIO.LOW)
+    GPIO.output(M2_STEP_PIN, GPIO.LOW)
+    GPIO.output(M3_STEP_PIN, GPIO.LOW)
+    GPIO.output(M4_STEP_PIN, GPIO.LOW)
